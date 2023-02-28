8f70c84a73e3d917763e87ad39da258c6117eca7
Geordan Sanchez <geordan62@gmail.com>
2023-02-27 21:54:40 -0500

    Playing with injections

--- src/main/java/com/example/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
+++ src/main/java/com/example/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -1,8 +1,59 @@
 package com.example.learnspringframework.examples.a1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class YourBusinessClass {
+
+    Dependency1 dependency1;
+
+    Dependency2 dependency2;
+
+/*
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter Injection " + dependency1);
+        this.dependency1 = dependency1;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter Injection " + dependency2);
+        this.dependency2 = dependency2;
+    }
+*/
+
+    //@Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("Constructor Injection ");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    @Override
+    public String toString() {
+        return "YourBusinessClass{" +
+                "dependency1=" + dependency1 +
+                ", dependency2=" + dependency2 +
+                '}';
+    }
+}
+
+@Component
+class Dependency1 {
+
+}
+
+@Component
+class Dependency2 {
+
+}
 
 @Configuration
 @ComponentScan
@@ -11,7 +62,8 @@ public class DependencyInjectionLauncherApplication {
     public static void main(String[] args) {
 
         try(var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class)) {
-
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println(context.getBean(YourBusinessClass.class));
         }
     }
 }
